4bc44a34f4dcd5a58b84a874a73e457856e5ceba
Akshara R <akshararamesh0512@gmail.com>
2025-08-09 01:05:11 +0530

    Updated firebase configuration

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,13 +1,15 @@
-// firebase.js - paste your Firebase web config here (compat mode used by frontend)
+// paste Firebase config here
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCELC91dEecbZtIIHtoqZ134KGjS1iSVuA",
+  authDomain: "bunkbank-9a272.firebaseapp.com",
+  projectId: "bunkbank-9a272",
+  storageBucket: "bunkbank-9a272.firebasestorage.app",
+  messagingSenderId: "528529720718",
+  appId: "1:528529720718:web:3c5a93f57dae43649a10df",
+  measurementId: "G-R6WK41P2TG"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
